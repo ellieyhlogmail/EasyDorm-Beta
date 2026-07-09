be75747568410140b185e46a1475e87c50b1dc5c
ellieyhlogmail <ellie.yh.lo@gmail.com>
2026-07-09 12:25:18 +0800

    Add files via upload
    First test, not finished

---
+++ recipes.js
@@ -0,0 +1,177 @@
+const recipes = {
+    "watermelon-shaved-ice":{
+        title: "Watermelon Shaved Ice",
+        categoryName: "Cold Foods",
+        categoryLink: "category1.html",
+        time: "5 to 10 minutes",
+        tools: "Grater, Freezer",
+        difficultyLevel: "Easy",
+        budget: "Cheap",
+        ingredients: [
+            "A Quarter of a Frozen Watermelon",
+            "A Drizzle Condensed Milk (optional)",
+            "1tsp of Lime Juice (optional)"
+        ],
+        steps:[
+            "Prepare your watermelon: cut your watermelon into stripes with the skin and freeze overnight",
+            "Check Your Watermelon and Prepare your bowl: Check that your watermelon is completly frozen. It should be hard and solid. Get a small bowl or serving dish ready.",
+            "Grate the watermelon: Take your solid block of frozen watermelon and carefully rub it against the fine side of your grater directly over your bowl. It will instantly turn into a fluffy, snow-like texture.",
+            "Add toppings: Drizzle the condensed milk over the top of your watermelon snow for extra sweetness and/or squeeze the juice out of your lime onto the shaved ice."
+        ],
+
+        mainImage:"https://samdoesherbest.com/wp-content/uploads/2025/07/Watermelon-Shaved-Ice-1-scaled.jpg",
+        prepImage:"https://theeastcoastkitchen.com/nitropack_static/JVwXKMegmYUHkLLRqdRKKXZNEMXPKMRc/assets/images/optimized/rev-b2a9676/theeastcoastkitchen.com/wp-content/uploads/2024/07/SHAVED-FROOZEN-FRUIT-17-min-1024x576.jpg",
+        video:"https://youtube.com/shorts/_auqRDSfmp4?si=823Zd6sLd-c8o1ES"
+    },
+
+    "lychee-yogurt-snack":{
+        title: "Frozen Lychee Yogurt Snack",
+        categoryName: "Cold Foods",
+        categoryLink: "category1.html",
+        time: "15 to 20 minutes",
+        tools: "Freezer, Piping Bag (optional)",
+        difficultyLevel: "Easy",
+        budget: "Cheap",
+        ingredients: [
+            "Lychee",
+            "Any type of Yogurt",
+            "Other fruits eg. berries (optional)"
+        ],
+        steps:[
+            "Prepare the lychee: carefully peel open the lychee. Take the seed out by cutting a hole at the top or by cutting the lychee in half. Place all of them faced up in a container.",
+            "Fill the lychee with yogurt: if you have a piping bag, open it and put the yogurt inside before squeezing it into the lychee. If not, open the lychee with your hands and use a spoon to place the yogurt inside. If you want, you can put a fruit of choice on top.",
+            "Put them in the freezer: close the lid of the container and carefully place the container in the freezer. Let the lychee freeze for at least 5 hours or overnight..",
+        ],
+
+            mainImage:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2PepoP-DkJDQtHHllkC1H5i_hl6cLjOCp3xYLLEL0WhHXNFh0QA72_7k&s=10",
+            prepImage:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrf7j0mlFaTvvKbsc_KhMjWHt5pqHvz0_bOeDey18STHzHUXmRIU3d_Jc&s=10",
+            video:"https://youtube.com/shorts/3phjf0S9nSc?si=hvFC64D1Yq4GGaWz"
+    },
+
+    "sprite-or-water-binghulu":{
+        title: "Sprite or Water Binghulu",
+        categoryName: "Cold Foods",
+        categoryLink: "category1.html",
+        time: "5 minutes",
+        tools: "Freezer and Skewers",
+        difficultyLevel: "Easy",
+        budget: "Cheap",
+        ingredients: [
+            "1 Bottle of sprite or water",
+            "Friuts of your choice (blueberries, grapes, strawberries, mandarins/tangerines)",
+            "A lot of ice cubes"
+        ],
+        steps:[
+            "Preparation: skewer the fruits however you like it and place them in the freezer overnight. Make your ice cubes in advance. Put your bottle of sprite in the freezer for 15-20 minutes prior.",
+            "Put your skewers in before scoop your ice cubes into the container and pouring the sprite/water in. Tip: use more ice cubes than sprite so that it cools the liquid down to create a shell.",
+            "Take the skewers out after 30-60 seconds and eat instantly to enjoy the perfect crunch!",
+        ],
+
+        mainImage:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPOXrbrudHLXp5Hkf1-23jIaKVy1So_fvL6A&s",
+        prepImage:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQU6pkpAiAQA1ZMw9TLzb4CeEBAdcJtMsTlMzeblzAuC0_zaNxMroT9wUg&s=10",
+        video:"https://youtube.com/shorts/8q61USP-uOo?si=wCRVzULvW6Y2h70t"
+    },
+
+    "mango-pudding":{
+        title: "Mango Pudding",
+        categoryName: "Cold Foods",
+        categoryLink: "category1.html",
+        time: "15 minutes of preperation",
+        tools: "Fridge and Kettle",
+        difficultyLevel: "Easy",
+        budget: "Cheap",
+        ingredients: [
+            "Mangoes",
+            "250 ml of evaporated milk",
+            "Mango flavored jelly powder or food coloring"
+        ],
+        steps:[
+            "Cut your mangoes into pieces.",
+            "Mix 100 ml of hot water and mango jelly powder/food coloring until it dissolves. ",
+            "Add 375 ml of water and wait for it to cool",
+            "Add 250ml of evaporated milk and the mangoes prepared earlier and stir",
+            "Pour the mixture into a container and put it in the fridge for at least 4 hours",
+        ],
+
+        mainImage:"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhCo8p_08ygRY6wRbCZKV3icz_dAV9vgTkJYdWm3I6jUsxBVkbap8EnE9SNbMEt2HxbvQygeJuTibt9Ka-CdQ8Ae7xuBSsEFoRygTaUxPTVPhja8O9Cux5vDH4JPq_YRvpY_BzpGYxThng/s1600/Mango+Pudding02.jpg",
+        prepImage:"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgo9MwbJTYzcbIKGX4on5KU9IMOOQK6iqxmaAZyAB8YhxQgq0vyNxCJ1Lg6A4kQD5qgLeZ2fPudIYm3k7-S7TiS3vynwWcM58LhWCUL1K_E9_pSWzU4bOeVVImRfwdv9r7lQkyhjk9OiFk/s1600/Mango+Pudding+Procedures.jpg",
+        video:"https://youtube.com/shorts/EVWbR7TqFGU?si=hhBUIhmXrCx2vtNN"
+    },
+
+    "3-color-jelly":{
+        title: "3 Color Jelly",
+        categoryName: "Cold Foods",
+        categoryLink: "category1.html",
+        time: "3-4 hours",
+        tools: "Fridge and Kettle",
+        difficultyLevel: "Easy",
+        budget: "Cheap",
+        ingredients: [
+            "3 different flavors of jelly powder or food coloring",
+            "Gelatin Powder",
+            "Sugar",
+            "Water"
+        ],
+        steps:[
+            "Seperate the gelatin into three seperate bowls and dissolve the first portion in 200 ml of hot water. Stir with a spoon until completly dissolved.",
+            "Add 250 ml of water after the water melts and cool it for 5-10 minutes. ",
+            "Use a strainer (optional) and pour the mixture into a container and refrigerate until it is set but not fully firm.",
+            "Repeat 2 more times but put it into the container while it is still hot",
+        ],
+
+        mainImage:"https://amish-heritage.org/wp-content/uploads/2022/10/20221031_103831-1-500x500.jpg",
+        prepImage:"https://balancingmotherhood.com/wp-content/uploads/2019/11/layered-jello.jpg",
+        video:"https://youtu.be/ByAk3IL08Jg?si=8A69AXkZQ1JSV1x-"
+    }
+
+};
+
+
+
+const params = new URLSearchParams(window.location.search);
+const recipeId = params.get("id")
+
+if (recipeId && recipes[recipeId]){
+    const recipe = recipes[recipeId];
+
+    document.title = recipe.title;
+    
+    document.getElementById("recipe-title").textContent = recipe.title;
+    document.getElementById("recipe-time").textContent = recipe.time;
+    document.getElementById("recipe-tools").textContent = recipe.tools;
+    document.getElementById("recipe-difficulty").textContent = recipe.difficultyLevel;
+    document.getElementById("recipe-budget").textContent = recipe.budget;
+
+    const mainImage = document.getElementById("main-image");
+    mainImage.src = recipe.mainImage;
+    mainImage.alt = recipe.title;
+
+    const prepImage = document.getElementById("prep-image");
+    prepImage.src = recipe.prepImage;
+    prepImage.alt = recipe.title + "preperation";
+
+    const videoLink = document.getElementById("video-link");
+    videoLink.href = recipe.video;
+
+    const ingredientsList = document.getElementById("ingredients");
+    recipe.ingredients.forEach(item => {
+        const li = document.createElement("li");
+        li.textContent = item;
+        ingredientsList.appendChild(li);
+    });
+
+
+    const recipeSteps = document.getElementById("recipe-steps");
+    recipe.steps.forEach(step => {
+        const li = document.createElement("li");
+        li.textContent = step;
+        recipeSteps.appendChild(li);
+    });
+    
+}
+
+else {
+    document.getElementById("recipe-title").textContent = "Recipe Not Found"
+    document.getElementById("ingredients").innerHTML = "<li>No recipe data available.</li>"
+    document.getElementById("recipe-steps").innerHTML = "<li>Please Return to the category page.</li>"
+}
