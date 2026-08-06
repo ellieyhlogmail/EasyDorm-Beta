e66695a9dbf43ff1767fb4066b672025fa5d3df2
ellieyhlogmail <ellie.yh.lo@gmail.com>
2026-08-06 12:37:10 +0800

    Add files via upload

---
+++ recipe_data.js
@@ -0,0 +1,315 @@
+const recipes = {
+    "watermelon-shaved-ice": {
+        title: "Watermelon Shaved Ice",
+        theme: "watermelon",
+        categoryName: "Freezer",
+        categoryLink: "category1.html",
+        time: "5 to 10 minutes",
+        tools: "Grater, Freezer",
+        difficultyLevel: "Easy",
+        budget: "Cheap",
+        ingredients: [
+            "A quarter of a frozen watermelon",
+            "A drizzle of condensed milk (optional)",
+            "1 tsp of lime juice (optional)"
+        ],
+        steps: [
+            "Prepare your watermelon: Cut your watermelon into strips with the skin on and freeze overnight.",
+            "Check your watermelon and prepare your bowl: Check that your watermelon is completely frozen. It should be hard and solid. Get a small bowl or serving dish ready.",
+            "Grate the watermelon: Take your solid block of frozen watermelon and carefully rub it against the fine side of your grater directly over your bowl. It will instantly turn into a fluffy, snow-like texture.",
+            "Add toppings: Drizzle the condensed milk over the top of your watermelon snow for extra sweetness and/or squeeze the juice out of your lime onto the shaved ice."
+        ],
+        mainImage: "https://lh3.googleusercontent.com/d/1sNEqjCLv3xhb6dCr7IdIF5g0Am_lLMJM",
+        prepImage: "https://lh3.googleusercontent.com/d/1S6xCvF0H_z0GYEVO3oeZ1ScB6lUMpPP2",
+        video:"",
+
+        stepImages: [
+            "image1.jpg",
+            "image2.jpg",
+            "image3.jpg"
+        ]
+    },
+
+    "lychee-yogurt-snack": {
+        title: "Frozen Lychee Yogurt Snack",
+        theme: "lychee",
+        categoryName: "Freezer",
+        categoryLink: "category1.html",
+        time: "15 to 20 minutes",
+        tools: "Freezer, Piping Bag (optional)",
+        difficultyLevel: "Easy",
+        budget: "Cheap",
+        ingredients: [
+            "Lychee",
+            "Any type of yogurt",
+            "Other fruits e.g. berries (optional)"
+        ],
+        steps: [
+            "Prepare the lychee: Carefully peel open the lychee. Take the seed out by cutting a hole at the top or by cutting the lychee in half. Place all of them facing up in a container.",
+            "Fill the lychee with yogurt: If you have a piping bag, open it and put the yogurt inside before squeezing it into the lychee. If not, open the lychee with your hands and use a spoon to place the yogurt inside. If you want, you can put a fruit of your choice on top.",
+            "Put them in the freezer: Close the lid of the container and carefully place the container in the freezer. Let the lychee freeze for at least 5 hours or overnight."
+        ],
+        mainImage: "https://lh3.googleusercontent.com/d/1NoSYAk9ZWpPJA5COEWYkOSIR0a77MlIy",
+        prepImage: "https://lh3.googleusercontent.com/d/1gMVoU_Mqwi1wAzZ3L8uHgYVuily4cgI0",
+        video:"",
+
+        stepImages: [
+            "image1.jpg",
+            "image2.jpg",
+            "image3.jpg"
+        ]
+    },
+
+    "sprite-or-water-binghulu": {
+        title: "Sprite or Water Binghulu",
+        theme: "sprite",
+        categoryName: "Freezer",
+        categoryLink: "category1.html",
+        time: "5 minutes",
+        tools: "Freezer and skewers",
+        difficultyLevel: "Easy",
+        budget: "Cheap",
+        ingredients: [
+            "1 bottle of Sprite or water",
+            "Fruits of your choice (blueberries, grapes, strawberries, mandarins/tangerines)",
+            "A lot of ice cubes"
+        ],
+        steps: [
+            "Preparation: Skewer the fruits however you like and place them in the freezer overnight. Make your ice cubes in advance. Put your bottle of Sprite in the freezer for 15 to 20 minutes beforehand.",
+            "Put your skewers in before scooping your ice cubes into the container and pouring the Sprite or water in. Tip: Use more ice cubes than Sprite so that it cools the liquid down to create a shell.",
+            "Take the skewers out after 30 to 60 seconds and eat instantly to enjoy the perfect crunch."
+        ],
+        mainImage: "https://lh3.googleusercontent.com/d/1SDiQ9wuTyHx8XD2O3sFri1skdnX7HtF-",
+        prepImage: "https://lh3.googleusercontent.com/d/1AwJFXUsPGa-wGhZqjyIkRg-yHPzPJvZ-",
+        video:"",
+
+        stepImages: [
+            "image1.jpg",
+            "image2.jpg",
+            "image3.jpg"
+        ]
+    },
+
+    "mango-pudding": {
+        title: "Mango Pudding",
+        theme: "mango",
+        categoryName: "Fridge",
+        categoryLink: "category2.html",
+        time: "15 minutes of preparation",
+        tools: "Fridge and kettle",
+        difficultyLevel: "Easy",
+        budget: "Cheap",
+        ingredients: [
+            "Mangoes",
+            "250 ml of evaporated milk",
+            "Mango-flavored jelly powder or food coloring"
+        ],
+        steps: [
+            "Cut your mangoes into pieces.",
+            "Mix 100 ml of hot water and mango jelly powder or food coloring until it dissolves.",
+            "Add 375 ml of water and wait for it to cool.",
+            "Add 250 ml of evaporated milk and the mangoes prepared earlier, then stir.",
+            "Pour the mixture into a container and put it in the fridge for at least 4 hours."
+        ],
+        mainImage: "https://lh3.googleusercontent.com/d/1U1Sjok9pAPQ3GYIOiPzvfd39Na3AxW-3",
+        prepImage: "https://lh3.googleusercontent.com/d/1naFpfpSUIr8TDWrZzAQ40uTocWcYSHyx",
+       video:"",
+
+        stepImages: [
+            "image1.jpg",
+            "image2.jpg",
+            "image3.jpg"
+        ]
+    },
+
+    "3-color-jelly": {
+        title: "3 Color Jelly",
+        theme: "jelly",
+        categoryName: "Fridge",
+        categoryLink: "category2.html",
+        time: "3 to 4 hours",
+        tools: "Fridge and kettle",
+        difficultyLevel: "Easy",
+        budget: "Cheap",
+        ingredients: [
+            "3 different flavors of jelly powder or food coloring",
+            "Gelatin powder",
+            "Sugar",
+            "Water"
+        ],
+        steps: [
+            "Separate the gelatin into three separate bowls and dissolve the first portion in 200 ml of hot water. Stir with a spoon until completely dissolved.",
+            "Add 250 ml of water after it dissolves and let it cool for 5 to 10 minutes.",
+            "Use a strainer (optional) and pour the mixture into a container, then refrigerate until it is set but not fully firm.",
+            "Repeat 2 more times, but pour each new layer into the container while it is still hot."
+        ],
+        mainImage: "https://lh3.googleusercontent.com/d/1W7ZJ-eeQxzKQ1atmDoRffmWDQtkJxmoZ",
+        prepImage: "https://lh3.googleusercontent.com/d/1_hQqE2nlhwXE9KvUce9fy5h0VcXZs2wM",
+        video:"",
+
+        stepImages: [
+            "image1.jpg",
+            "image2.jpg",
+            "image3.jpg"
+        ]
+    },
+
+    "watermelon-yakult-smoothie": {
+        title: "Watermelon Yakult Smoothie",
+        theme: "yakult",
+        categoryName: "Blender",
+        categoryLink: "category3.html",
+        time: "5 to 10 minutes",
+        tools: "Freezer and blender",
+        difficultyLevel: "Easy",
+        budget: "Cheap",
+        ingredients: [
+            "Fresh or frozen watermelon (use frozen watermelon for a thicker texture)",
+            "1 bottle of Yakult",
+            "Ice cubes (for those using fresh watermelon)",
+            "1 can of Sprite (optional)"
+        ],
+        steps: [
+            "Cut your watermelon into slices and freeze overnight if you want a thicker smoothie.",
+            "Prepare your ingredients: Put your watermelon slices, a bottle of Yakult, one can of Sprite (optional), and ice cubes (optional) into the blender.",
+            "Blend until smooth."
+        ],
+        mainImage: "https://lh3.googleusercontent.com/d/1R5w1NfEPUFc4TsTbWnbV5mVSOykzkpl8",
+        prepImage: "https://lh3.googleusercontent.com/d/18-xj5g6HTuy5vMj8UVJornrerKztKdfy",
+        video:"",
+
+        stepImages: [
+            "image1.jpg",
+            "image2.jpg",
+            "image3.jpg"
+        ]
+    },
+
+    "berry-sorbet": {
+        title: "Smashed Berry Sorbet",
+        theme: "berry",
+        categoryName: "Freezer",
+        categoryLink: "category1.html",
+        time: "5 to 10 minutes",
+        tools: "Freezer",
+        difficultyLevel: "Easy",
+        budget: "Cheap",
+        ingredients: [
+            "Frozen berries",
+            "Milk"
+        ],
+        steps: [
+            "Freeze your berries overnight beforehand.",
+            "Put your berries into a bowl and add a bit of milk.",
+            "Chop and mix until smooth, and add more milk if needed."
+        ],
+        mainImage: "https://lh3.googleusercontent.com/d/1aQW7ySiOqswZPANT80g39XVEp5wzSAau",
+        prepImage: "https://lh3.googleusercontent.com/d/1CoF_wLTnNDASsZw0J7EVaZWItCPXr2xL",
+        video:"",
+
+        stepImages: [
+            "image1.jpg",
+            "image2.jpg",
+            "image3.jpg"
+        ]
+    },
+    
+    "watermelon-lychee-soda":{
+       title: "Watermelon Lychee Soda",
+        theme: "soda",
+        categoryName: "Blender and Drinks",
+        categoryLink: "category3.html",
+        time: "5-10 minutes",
+        tools: "Blender and Strainer (optional)",
+        difficultyLevel: "Easy",
+        budget: "Cheap",
+        ingredients: [
+            "100g of Watermelon",
+            "60g of Lychee",
+            "Soda Water",
+        ],
+        steps:[
+            "Prepare your Fruits: Cut your watermelon into cubes and take the pit out of your lychees.",
+            "Put your fruit into a blender and blend untill smooth.",
+            "Pour the blended mixture into a cup and top with a little bit of sparkling water.",
+        ],
+
+         mainImage:"https://lh3.googleusercontent.com/d/1mOYqLFG7OLDlYL6_6csuEov9aLDNq8D9",
+        prepImage:"https://lh3.googleusercontent.com/d/1qNSrMIUpqStZOScPpuRFCBmm2FYC00Fz",
+        video:"",
+
+        stepImages: [
+            "image1.jpg",
+            "image2.jpg",
+            "image3.jpg"
+        ]
+
+
+},
+
+"mango-lemonade":{
+       title: "Mango Lemondae",
+        theme: "lemonade",
+        categoryName: "Blender and Drinks",
+        categoryLink: "category3.html",
+        time: "5-10 minutes",
+        tools: "Blender and Strainer",
+        difficultyLevel: "Easy",
+        budget: "Cheap",
+        ingredients: [
+            "1 Whole Mango",
+            "1/4 of a Lemon",
+            "Condensed Milk",
+            "400-500mL of Water",
+            "Ice For Serving (optional)"
+        ],
+        steps:[
+            "Prepare your Fruits: Cut your mango and lemon into cubes/slices",
+            "Put your fruit and a drizzle of condensed milk into a blender and blend untill smooth.",
+            "Strain the blended mixture and pour into a cup with ice.",
+        ],
+
+         mainImage:"https://lh3.googleusercontent.com/d/13O3ojgb18lds5HXk8Nx3AIGUOI463lT1",
+        prepImage:"https://lh3.googleusercontent.com/d/1p_JeVFY2x4rvkqTuZNkJ23JcZ8NMZL0R",
+        video:"",
+
+        stepImages: [
+            "https://lh3.googleusercontent.com/d/1hmMqe1ms3xMCfezOqI_T2Use3PYC-nCV",
+            "https://lh3.googleusercontent.com/d/1suCPT1Qe8VAyxCmhELWiPmrEpKHlHmFa"
+        ]
+
+
+},
+"milk-coated-blueberries":{
+       title: "Milk Coated Blueberries",
+        theme: "blueberries",
+        categoryName: "Freezer",
+        categoryLink: "category1.html",
+        time: "2-3 minutes",
+        tools: "Freezer",
+        difficultyLevel: "Easy",
+        budget: "Cheap",
+        ingredients: [
+            "Frozen Bluberries",
+            "Milk",
+        ],
+        steps:[
+            "Freeze your blueberries overnight beforehand",
+            "Put your berries into a bowl and add a bit of milk",
+            "Mix until all the blueberries are coated and add more milk if not enough",
+        ],
+
+         mainImage:"https://lh3.googleusercontent.com/d/17_y1c7aCO_37Ohwzkz9fgybJLiep05ch",
+        prepImage:"https://lh3.googleusercontent.com/d/1CoF_wLTnNDASsZw0J7EVaZWItCPXr2xL",
+        video:"",
+
+        stepImages: [
+            "image1.jpg",
+            "image2.jpg",
+            "image3.jpg"
+        ]
+
+
+},
+
+};
